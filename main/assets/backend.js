28401fd657e27d94d3afcdc183e4de9896cf8c36
andy-and-terry <248389669+andy-and-terry@users.noreply.github.com>
2026-08-24 18:07:19 +0800

    Update backend.js

--- main/assets/backend.js
+++ main/assets/backend.js
@@ -1,5 +1,16 @@
 //backend.js v0.1.0
 
 const message = localStorage.getItem('message');
-const url = `YOUR_VAL_URL/?message=${message}&userid=${userid}`;
-const output = await fetch(url).then(res => res.text());
+function chat(){
+  const res = await fetch("https://andyandterry--3993b1649f9e11f19dda1607ee4eb77e.web.val.run", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      id: userid,
+      model: "gpt-oss:20b",
+      message: message
+    })
+  });
+  const data = await res.json();
+  const output = data.reply;
+}
